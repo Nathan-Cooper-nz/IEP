1dc492d309fc78bb688a80e04e101a29d5dd0be0
Pri Bhula <pridotbhula@gmail.com>
2016-05-25 11:57:18 +1200

    Added notebook widget

--- Layout.py
+++ Layout.py
@@ -1,8 +1,11 @@
 from Tkinter import *
+import ttk
 
 class Application(Frame):
     def __init__(self, master):
         frame = Frame(master)
+
+        container = Tkinter.Frame(self)
         frame.pack              #Make Visible
 
         sectionWd = 25
@@ -34,11 +37,13 @@ class Application(Frame):
         curRow+=1
 
         Label(text="Oscilloscope", relief=RIDGE,width=100,height=30).grid(row=0,column=0,rowspan=curRow)
+        container.pack(side="top",fill="both",expand=True)
+        
         Label(text="TABS", relief=RIDGE,width=100,height=10).grid(row=curRow,column=0)
 
         
 root = Tk()                  #Create Application Window
-
+#tabs = ttk.notebook(root,side=TOP)
 app = Application(root)
 
 root.mainloop()
