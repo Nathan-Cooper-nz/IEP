9d1d0a2a60e02c03cf2f8cb0845789b85a678686
Tyler <nztyler@hotmail.co.nz>
2016-06-05 06:23:16 +1200

    Added some buttons to the oscilloscope display + option changes

--- src/main/OscilloscopeDisplay.java
+++ src/main/OscilloscopeDisplay.java
@@ -5,12 +5,80 @@ import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class OscilloscopeDisplay extends JPanel{
 
+	private boolean channelOneOn = false;
+	private boolean channelTwoOn = false;
+	
     public OscilloscopeDisplay(){
-    	add(new JLabel("Oscilloscope display"));
     	Border border = LineBorder.createGrayLineBorder();
         setBorder(border);
+        
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        
+        //initialise the panels
+        JPanel displayPanel = getDisplayPanel();
+        JPanel buttonPanel = getButtonPanel();
+        
+        //add the oscilloscope display and save button
+        gbc.gridx = 0;
+        gbc.weightx = 0.9;
+        gbc.weighty = 1;
+        add(displayPanel, gbc);
+        gbc.gridx = 1;
+        gbc.weightx = 0.1;
+        add(buttonPanel, gbc);
     }
+    
+    public JPanel getDisplayPanel(){
+    	JPanel panel = new JPanel();
+    	panel.add(new JLabel("Oscilloscope display"));
+    	return panel;
+    }
+    
+    public JPanel getButtonPanel(){
+    	JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        JButton saveButton = new JButton("Save");	//screenshot
+        saveButton.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent e){
+        		System.out.println("Save button pressed");
+        		channelOneOn = false;
+        		channelTwoOn = false;
+        	}
+        });
+        ButtonGroup channel = new ButtonGroup();
+        JButton channelOne = new JButton("CH1");
+        channelOne.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent e){
+        		System.out.println("First channel selected");
+        		channelOneOn = true;
+        		channelTwoOn = false;
+        		
+        	}
+        });
+        JButton channelTwo = new JButton("CH2");
+        channelTwo.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent e){
+        		System.out.println("Second channel selected");
+        		channelOneOn = false;
+        		channelTwoOn = true;
+        	}
+        });
+        Dimension btnDimension = new Dimension(60,40);
+        channelOne.setPreferredSize(btnDimension);
+        channelTwo.setPreferredSize(btnDimension);
+        channel.add(channelOne);
+        channel.add(channelTwo);
+        buttonPanel.add(saveButton);
+        buttonPanel.add(Box.createRigidArea(new Dimension(0,20))); //put a gap between the buttons
+        buttonPanel.add(channelOne);
+        buttonPanel.add(channelTwo);
+        return buttonPanel;
+    }
+    
 }
--- src/main/OscilloscopeOptions.java
+++ src/main/OscilloscopeOptions.java
@@ -4,32 +4,98 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 public class OscilloscopeOptions extends JTabbedPane {
 
+	/*
+	public double timePerDiv;
+	private double triggerLevel;
+	
+	//CHANNEL ONE
+	private double voltageOne;
+	private boolean isOnOne;
+	private Coupling couplingOne;
+	
+	//CHANNEL TWO
+	private double voltageTwo;
+	private boolean isOnTwo;
+	private Coupling couplingTwo;
+	*/
+	
     public OscilloscopeOptions(){        
-        addTab("Volts/Div", getVoltsPerDiv());
-        addTab("Trigger", getTrigger());
-        addTab("Time/Div", getTimePerDiv());
+        addTab("Volts/Div shit", getVoltsPanel());
+        addTab("Trigger shit", getTriggerPanel());
+        addTab("Cursor shit", getCurserPanel());
         
     	Border border = LineBorder.createGrayLineBorder();
         setBorder(border);
     }
     
-    public JPanel getVoltsPerDiv(){
+    //=========== Voltage ===========//
+    public JPanel getVoltsPanel(){
+    	JPanel panel = new JPanel();
+    	
+    	JPanel left = getSubVoltagePanel("Channel 1");
+    	JPanel right = getSubVoltagePanel("Channel 2");
+        
+    	panel.add(left);
+    	panel.add(right);
+    	
+    	return panel;
+    }
+    
+    /*
+     * 
+     */
+    public JPanel getSubVoltagePanel(String name){
+    	JPanel panel = new JPanel();
+    	panel.setLayout(new GridBagLayout());
+    	GridBagConstraints gbc = new GridBagConstraints();
+    	gbc.gridwidth = GridBagConstraints.REMAINDER;
+    	
+    	JLabel text = new JLabel(name);
+    	//slider with min , max 30, default 5
+    	JSlider slider = new JSlider(0, 30, 5);
+    	JButton auto = new JButton("Autoset");
+    	auto.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent e){
+        		System.out.println("Autoset button pressed");
+        	}
+        });
+    	
+    	panel.add(text, gbc);
+        panel.add(Box.createRigidArea(new Dimension(0,10)), gbc); //put a gap between the buttons
+    	panel.add(auto, gbc);
+    	panel.add(slider, gbc);
+    	return panel;
+    }
+    
+  //=========== Triggering ===========//
+    public JPanel getTriggerPanel(){
     	JPanel panel = new JPanel();
     	return panel;
     }
     
-    public JPanel getTrigger(){
+  //=========== Curser ===========//
+    public JPanel getCurserPanel(){
     	JPanel panel = new JPanel();
     	return panel;
     }
+   
     
-    public JPanel getTimePerDiv(){
-    	JPanel panel = new JPanel();
-    	return panel;
+    /**
+     * This just makes it clearer that there should always be a specific
+     * type of coupling
+     * @author nztyler
+     *
+     */
+    public enum Coupling {
+    	AC, DC, GROUND
     }
 }
