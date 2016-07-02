080c160187f126d734e69209b64cd30e3e012f14
Pri Bhula <pridotbhula@gmail.com>
2016-07-03 00:12:12 +1200

    Changed funcgen side and playing around with a graph library

--- src/main/FunctionDisplay.java
+++ src/main/FunctionDisplay.java
@@ -4,12 +4,26 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.Graphics;;
 
-public class FunctionDisplay extends JPanel {
+public class FunctionDisplay extends JPanel{
 
-    public FunctionDisplay(){
-    	add(new JLabel("Function display"));
-    	Border border = LineBorder.createGrayLineBorder();
-        setBorder(border);
-    }
+	public FunctionDisplay(){
+		add(new JLabel("Function display"));
+		Border border = LineBorder.createGrayLineBorder();
+		setBorder(border);
+		//GraphCanvas gc = new GraphCanvas();
+		//gc.paint(new Graphics());
+		//add(getDisplay());
+	}
+
+	public JPanel getDisplay(){
+		JPanel panel = new JPanel();
+		int w = panel.getWidth();
+		int h = panel.getHeight();
+		int startx=0;
+
+		return panel;
+
+	}
 }
