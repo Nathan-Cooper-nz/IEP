25ac35a691a9613b71ad24d978d7ad4fc702e46f
Nathan Cooper <nathan.cooper.nzl@gmail.com>
2016-09-16 12:54:08 +1200

    Removed Print Statement that flooded python backend console with uneeded information

--- BackEnd/server.py
+++ BackEnd/server.py
@@ -60,9 +60,7 @@ class Server(threading.Thread):
         while True:
             if(not self.sendQueue.empty()):
                 message = self.sendQueue.get()
-                # print("Sending Message:",message)
                 encoded_msg = message.encode()
-                print(self.client)
                 self.socket.sendto(encoded_msg, self.client)
 
 
